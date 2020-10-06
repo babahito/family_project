3fb106c38dd785616ad70a65b0de8efdb4b56465
babahito <babahito@gmail.com>
2020-10-06 12:23:06 +0900

    a

--- resources/views/auth/deactive.blade.php
+++ resources/views/auth/deactive.blade.php
@@ -3,24 +3,7 @@
 
 
 <main>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div>
-
-
-                <div class="card-body text-center">
-                    <form method="POST" action="{{ route('deactive') }}">
-                        @csrf
-                        <h2>{{ __('このアカウントはご利用できなくなります') }}</h2>
-                        <p>{{ __('よろしければ、ボタンをclickしてください') }}</p>
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('退会する') }}
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+aa
 </main>
 
 @endsection
