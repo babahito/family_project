f132876777f45c53565b7e9cd2016fe9b2195f08
babahito <babahito@gmail.com>
2020-10-11 11:01:25 +0900

    画像にもどし

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -111,8 +111,7 @@
 
                             <div class="card">
                                 <!-- <img src="data:image/png;base64,{{ $item->photo }}"  class="card-img-top"  style="width:100%; height: 180px;object-fit: cover;"> -->
-                                <!-- <img class="rounded" src="{{-- Storage::disk('s3')->url($item->photo) --}}" fclass="card-img-top"  style="width:100%; height: 180px;object-fit: cover;"> -->
-                                <video class="rounded" src="{{ Storage::disk('s3')->url($item->photo) }}" fclass="card-img-top"  style="width:100%; height: 180px;object-fit: cover;">
+                                <img src="{{ Storage::disk('s3')->url($item->photo) }}" fclass="card-img-top"  style="width:100%; height: 180px;object-fit: cover;">
                                 
                                     <div class="card-body">
                                     <p class="note_title"><a href="{{ url("/post/" . $item->id) }}" class="stretched-link text-dark">{{ $item->title}}</a></p>
