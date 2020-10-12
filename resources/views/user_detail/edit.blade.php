3acdfb0df1cc8e88f32496570d39c1b78a11f9fe
babahito <babahito@gmail.com>
2020-10-12 14:46:50 +0900

    ユーザー詳細

--- resources/views/user_detail/edit.blade.php
+++ resources/views/user_detail/edit.blade.php
@@ -29,6 +29,77 @@
 <main>
     <h2>My PAGE</h2>
 
+              <!-- 本人紹介 -->
+
+                        <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-3 mb-3 profile_box">
+                            <figure class="effect-color" style="margin:0 auto;">
+                            <!-- 登録がない場合 -->
+                            @if(!isset($user_detail->photo))
+                            <img src="{{ asset('/assets/images/noimage.png') }}" class="person_icon" style="margin:5px;">
+                            <!-- 登録がある場合 -->
+                            @else
+                                <!-- <img src="data:image/png;base64,{{-- $user_detail->photo--}}" class="person_icon" style="margin:5px;"> -->
+                                <img src="{{ Storage::disk('s3')->url($user_detail->photo) }}"  class="person_icon" style="margin:5px;">
+                            
+                            @endif
+                            </figure>
+
+
+                            </div>
+                            <div class="col-xs-8">
+                                <div class="card-body">
+                                    <h4 class="cartitle">{{ $auth->name }}</h4>
+                                    
+                                    <p class="card-text">{{ $user_detail->comment}}</p>
+                                </div>
+                            </div>
+                        </div>
+            <!-- end -->
+
+
+
+    
+
+            <div>
+            <div>
+                @if ($errors->any())
+                    <ul class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
+    
+                            <form method="POST" action="/user_detail/{{ $user_detail->id }}">
+                                    {{ csrf_field() }}
+                                    {{ method_field("PUT") }}
+
+                                    <div>
+                                        <input class="form-control" required="required" name="user_id" type="hidden" id="user_id" value="{{$user_detail->user_id}}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="comment">自己紹介</label>
+                                        <textarea name="comment" row="3" class="form-control">{{$user_detail->comment}}</textarea>
+                                    </div>
+                                    <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="birthday">誕生日</label>
+                                            <input class="form-control" name="birthday" type="date" id="birthday" value="{{$user_detail->birthday}}">
+                                    </div>
+    
+                                    </div>
+                                    <div>
+                                        <input type="submit" value="マイページ変更"　class="pink_btn">
+                                    </div>
+                                </form>
+                                
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 
 
 
