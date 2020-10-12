8b629294fb3480d78a0ca4d7684975324a1917dd
babahito <babahito@gmail.com>
2020-10-12 18:35:12 +0900

    新規ログインの修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,12 @@
 <main>
     <div class="main_user">
         ご利用ありがとうございます<br>
-        つづきまして、ユーザー詳細の設定をお願いします。<br>
+        つづきまして、ユーザー詳細の設定をお願いします。
+        <div style="margin-top:20px;">
         <butto class="pink_btn mt-5">
             <a href="{{ action('UserDetailsController@create') }}">ユーザー詳細画面</a>
         </button>
+        </div>
     </div>
 </main>
 @endsection
